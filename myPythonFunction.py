0a0e2c8873c1bcba8257290570cc4046d089c5d3
Thompson <gmthomp@b0be83757e4e.ant.amazon.com>
2023-12-01 12:45:26 -0800

    all

--- myPythonFunction.py
+++ myPythonFunction.py
@@ -3,6 +3,7 @@ import json
 def handler(event, context):
     # TODO implement
     return {
+        print('hello')
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
