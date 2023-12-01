624b668fbfbd57405a57d1eacff5841960ce8d1b
Grant Thompson <thom6171@eduhsd.k12.ca.us>
2023-12-01 13:00:06 -0800

    Update myPythonFunction.py

--- myPythonFunction.py
+++ myPythonFunction.py
@@ -3,7 +3,7 @@ import json
 def handler(event, context):
     # TODO implement
     return {
-        print('hello123')
+        print('hello12345')
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
